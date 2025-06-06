82a47bd21487b9d88531c271a4f8568ada931f77
jismayScarlet <jismayscarlet@gmail.com>
2025-06-06 10:51:19 +0800

    可再利用物保存

---
+++ app/src/androidTest/java/com/example/attendant_project/JsonMessageTool.java
@@ -0,0 +1,13 @@
+package com.example.attendant_project;
+
+public class JsonMessageTool {
+    //                                .put("properties", new JSONObject()
+//                                        .put("mood", new JSONObject()
+//                                                .put("type", "string")
+//                                                .put("description", "使用者的心情")
+//                                        )
+//                                        .put("contextOfMood",new JSONObject()
+//                                                .put("type","string")
+//                                                .put("description","情緒發生的情境")
+//                                        )
+}
